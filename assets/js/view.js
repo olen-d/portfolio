e42c468246afe74f143248a719d18d0bef174604
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-01-22 01:38:57 -0500

    Let's see if the HB works

--- assets/js/view.js
+++ assets/js/view.js
@@ -13,7 +13,7 @@ retrieveAbout = new Promise ((resolve, reject) => {
         if(sv !== null) {
             aboutItems.forEach((item) => {
                 contextString += `"${item}:` + sv[item] + ", ";
-                console.log("------ ",contextString);
+                console.log("[[]]]]", userId, "------ ",contextString);
             });
             contextString = contextString.slice(0,-2);
             contextString += "}";
