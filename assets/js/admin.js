a179c9246142b412836ba9a2ee45ccaea24cc693
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-01-22 01:14:04 -0500

    Let's see if the HB works

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,27 +1,3 @@
-//  Initialize Firebase
-const config = {
-    apiKey: "AIzaSyC-KLq7IYGv31TskNl4Bs7tXsqxqAB6dTE",
-    authDomain: "devportfolio-29145.firebaseapp.com",
-    databaseURL: "https://devportfolio-29145.firebaseio.com",
-    projectId: "devportfolio-29145",
-    storageBucket: "devportfolio-29145.appspot.com",
-    messagingSenderId: "803504641717"
-};
-
-firebase.initializeApp(config);
-
-let db = firebase.database();
-let aboutRef = db.ref("/about");
-let projsRef = db.ref("/projects");
-let skillsRef = db.ref("/skills");
-let themesRef = db.ref("themes");
-let usersRef = db.ref("/users");
-
-let displayName = "";
-let photoURL = "";
-let userId = "";
-let currentGroup = "";
-
 let loginBtn = document.getElementById("loginBtn");
 let logoutBtn = document.getElementById("logoutBtn");
 let aboutBtn = document.getElementById("aboutBtn");
@@ -44,93 +20,6 @@ let aboutItems = [
 ];
 
 const aboutData = new Object();
-
-firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-        // User is signed in
-        loginBtn.style.display = "none";
-        logoutBtn.style.display = "inline";
-
-        displayName = user.displayName;
-        photoURL = user.photoURL;
-        userId = user.uid;
-//console.log("---- ", displayName);
-//console.log(photoURL);
-//console.log(userId);
-        let providerData = user.providerData;
-
-        // See if the user exists
-        if (auth.userExists(userId)) {
-            const lastLogin = { lastLogin: firebase.database.ServerValue.TIMESTAMP };
-            db.ref(`/users/${userId}`).set(lastLogin, (error) => {
-                (error ? console.log("Errors handled " + error) : console.log("Last login successfully updated. "));
-            });
-        } else {
-            // User doesn't exist, add them to the Firebase Users
-            const userData = {
-                "name": displayName,
-                "photo": photoURL,
-                "joined": firebase.database.ServerValue.TIMESTAMP,
-                "lastLogin": firebase.database.ServerValue.TIMESTAMP
-            }
-            console.log("///--/// ",userId);
-            auth.addUser(userId, userData);
-        }
-    } else {
-        //  Not signed in
-        logoutBtn.style.display = "none";
-        loginBtn.style.display = "inline";
-    }
-});
-
-const auth = {
-    login() {
-        let provider = new firebase.auth.GithubAuthProvider();
-        firebase.auth().signInWithRedirect(provider);
-    },
-
-    logout() {
-//console.log(";;;;;; LOGOUT fireD")
-        firebase.auth().signOut();
-    },
-
-    addUser(userId, userData) {
-        if (!auth.userExists(userId)) {
-            console.log("ADD USER FIRED");
-            db.ref(`/users/${userId}`).set(userData, (error) => {
-                (error ? console.log("Errors handled " + error) : console.log("User successfully added to the database. "));
-            });
-        }
-    },
-
-    userExists(userId) {
-        usersRef.child(userId).once("value", (snapshot => {
-            let exists = (snapshot.val() !== null);
-//console.log("++--++ ",exists);
-            return exists;
-        }));   
-    }
-}
-
-//  Firebase Authentication
-firebase.auth().getRedirectResult().then((result) => {
-    if (result.credential) {
-        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-        const token = result.credential.accessToken;
-        // ...
-    }
-    // The signed-in user info.
-    const user = result.user;
-    }).catch(function(error) {
-    // Handle Errors here.
-    let errorCode = error.code;
-    let errorMessage = error.message;
-    // The email of the user's account used.
-    let email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    let credential = error.credential;
-    // ...
-});
 
 const admin = {
     showGroup(group) {
@@ -158,19 +47,7 @@ const data = {
                 aboutItems.forEach((item) => {
                     document.getElementById(item).value = sv[item];
                 });
-                // document.getElementById("firstName").value = sv.firstName;
-                // document.getElementById("lastName").value = sv.lastName;
-                // document.getElementById("title").value = sv.title;
-                // document.getElementById("email").value = sv.email;
-                // document.getElementById("tel").value = sv.tel;
-                // document.getElementById("address").value = sv.address;
-                // document.getElementById("street").value = sv.street;
-                // document.getElementById("city").value = sv.city;
-                // document.getElementById("state").value = sv.state;
-                // document.getElementById("zip").value = sv.zip;
-                // document.getElementById("bio").value = sv.bio;
             }
-
         // Handle any errors
         }, (errorObject) => {
             console.log("Errors handled: " + errorObject.code);
@@ -182,7 +59,7 @@ const data = {
         if(validate.aboutForm()) {
             let ref = db.ref(`/about/${userId}`)
 
-            aboutItems.forEach((key) => { //console.log("--- ",key); console.log("+++ ", document.getElementById("firstName"));
+            aboutItems.forEach((key) => {
                 value = document.getElementById(key).value.trim();
                 aboutData[key] = value;
             });
---
+++ assets/js/view.js
@@ -0,0 +1,37 @@
+let source = document.getElementById("portfolio-template").innerHTML; 
+let template = Handlebars.compile(source);
+let imagePage = document.getElementById("portfolio-page"); 
+let contextString = "{";
+
+
+retrieveAbout = new promise ((resolve, reject) => {
+    let ref = db.ref(`/about/${userId}`)
+        
+    ref.once("value", function(snapshot) {
+        let sv = snapshot.val();
+
+        if(sv !== null) {
+            aboutItems.forEach((item) => {
+                contextString += `"${item}:` + sv[item] + ", ";
+            });
+        }
+        contextString = contextString.slice(0,-2);
+        contextString += "}";
+        });
+    resolve(contextString);
+
+    // Handle any errors
+    }, (errorObject) => {
+        console.log("Errors handled: " + errorObject.code);
+    reject("Fail");
+});  
+
+
+retrieveAbout
+    .then((contextString) => {
+        let context = JSON.parse(contextString);
+        let html = template(context);
+    });
+
+
+
