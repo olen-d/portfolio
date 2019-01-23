f54e466ea9b7bba78fa8ab5b2d8297585a525235
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-01-22 20:56:17 -0500

    Everything working, including the promises.

--- assets/js/view.js
+++ assets/js/view.js
@@ -6,52 +6,31 @@ let contextString = "{";
 
 userId = "aTSIOrIboZWVaYv5RUwMWbbpZbx1";
 
+let ref = db.ref(`/about/${userId}`)
+    
+ref.once("value").then(function(snapshot) {
+    let sv = snapshot.val();
 
-    let ref = db.ref(`/about/${userId}`)
-        
-    ref.once("value").then(function(snapshot) {
-        let sv = snapshot.val();
-
-        if(sv !== null) {
-            const aboutPromise = new Promise((resolve,reject) => {
-                aboutItems.forEach((item) => {
-                    contextString += `"${item}":` + `"${sv[item]}", `;
-    //console.log("[[]]]]", userId, "------ ",contextString);
-                });
-                if(contextString.length > 1) {
-                    resolve(contextString);
-                } else {
-                    reject(Error(500));
-                }
+    if(sv !== null) {
+        const aboutPromise = new Promise((resolve,reject) => {
+            aboutItems.forEach((item) => {
+                contextString += `"${item}":` + `"${sv[item]}", `;
             });
+            if(contextString.length > 1) {
+                resolve(contextString);
+            } else {
+                reject(Error(500));
+            }
+        });
 
-            aboutPromise.then((result) => {
-                contextString = result.slice(0,-2);
-                contextString += "}";
-            });
-        }
-    });
-            
+        aboutPromise.then((result) => {
+            contextString = result.slice(0,-2);
+            contextString += "}";
 
-
-
-    // Handle any errors
-    // }, (errorObject) => {
-    //     console.log("Errors handled: " + errorObject.code);
-    // reject("Fail");
-// });  
-
-
-// retrieveAbout
-//     .then((contextString) => {
-//         console.log("[[[]]] ",contextString);
-//         let context = JSON.parse(contextString);
-//         let html = template(context);
-//     });
-
-setTimeout(() => {
-//console.log("[[[]]] ",contextString);
-    let context = JSON.parse(contextString);
-    let html = template(context);
-    imagePage.innerHTML += html
-},2000);
+            // Render the about template
+            let context = JSON.parse(contextString);
+            let html = template(context);
+            imagePage.innerHTML += html
+        });
+    }
+});
