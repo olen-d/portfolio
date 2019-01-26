fda17cb202ba7ab74b3baadbe16962c492880d91
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-01-26 01:42:56 -0500

    Everything is working, but still not finished

--- assets/js/view.js
+++ assets/js/view.js
@@ -2,58 +2,42 @@ let source = document.getElementById("portfolio-template").innerHTML;
 let template = Handlebars.compile(source);
 let imagePage = document.getElementById("portfolio-page"); 
 
-let contextString = "{";
+let contextString = ``;
+
 
 userId = "aTSIOrIboZWVaYv5RUwMWbbpZbx1";
 
-let refAbout = db.ref(`/about/${userId}`)
-    
+let refAbout = db.ref(`/about/${userId}`)   
 refAbout.once("value").then((snapshot) => {
     let sv = snapshot.val();
 
     const aboutPromise = new Promise((resolve,reject) => {
+        contextString = `{`;
         aboutItems.forEach((item) => {
             contextString += `"${item}": ` + `"${sv[item]}", `;
         });
-        if(contextString.length > 1) {
-            resolve(contextString);
-        } else {
-            reject(Error(500));
-        }
+        resolve(contextString);
     });
-
-    aboutPromise.then((result) => {
-        let refProjects = db.ref(`/projects/${userId}`)
-        refProjects.orderByChild("projectTitle").on("child_added", (snapshot) => {
-            //let sk = snapshot.ref.key
-            let psv = snapshot.val();
-            contextString += `"projects: {"`
-
-            //const projectsPromise
-            projectItems.forEach((item) => {
-                contextString += `"${item}": ` + `"${psv[item]}", `
-                //console.log(sk);
-                console.log(`"${item}": ` + `"${psv[item]}", `);
-            });
-        });
-        
-    }).then((result) => {
-        contextString = contextString.slice(0,-2);
-        contextString += "}";
-
-        // Render the about template
-        let context = JSON.parse(contextString);
-        let html = template(context);
-        imagePage.innerHTML += html
-        
-    });
-   
 });
 
-// contextString = result.slice(0,-2);
-//         contextString += "}";
-
-        // Render the about template
-        // let context = JSON.parse(contextString);
-        // let html = template(context);
-        // imagePage.innerHTML += html
+let refProjects = db.ref(`/projects/${userId}`);
+refProjects.orderByChild("projectTitle").on("value", (snapshot) => {
+    contextString += `"projects": [{`;
+    const projectsPromise = new Promise((resolve,reject) => {
+        snapshot.forEach((childSnapShot) => {
+            let ck = childSnapShot.key;
+            let cv = childSnapShot.val();
+            projectItems.forEach((item) => {
+                contextString += `"${item}": ` + `"${cv[item]}", `
+            });
+            contextString = contextString.slice(0,-2);
+            contextString += `}, {`
+            resolve(contextString);
+        });
+    });
+    contextString = contextString.slice(0,-3);
+    contextString += "] }";
+    let context = JSON.parse(contextString);
+    let html = template(context);
+    imagePage.innerHTML += html
+});
